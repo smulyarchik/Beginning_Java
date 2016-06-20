0517c4e5b136a59a6acecfb268875dd3188f4dea
smulyarchik <s.muliarchyk@gmail.com>
2016-06-20 07:41:11 -0700

    Add 'MorePrimes

---
+++ Chapter 4/WeatherFan/WeatherFan.java
@@ -0,0 +1,24 @@
+public class WeatherFan{
+	public static void main(String[] args){
+		float[][] temperature = new float[10][365];	// Temperature array.
+		
+		// Generate random temperatures.
+		for(int i = 0; i < temperature.length; ++i){
+			for(int j = 0; j < temperature[i].length; ++j){
+				temperature[i][j] = (float)(45.0 * Math.random() - 10);
+			}
+		}
+		
+		// Calculate the average per location.
+		for(int i = 0; i < temperature.length; ++i){
+			float average = 0.0F;	// Place to store the average.
+			
+			for(int j = 0; j < temperature[i].length; ++j){
+				average += temperature[i][j];
+			}
+			
+			// Output the average temperature for the current location.
+			System.out.println("Average temperature at location " + (i + 1) + " = " + average / (float)temperature[i].length);
+		}
+	}
+}
